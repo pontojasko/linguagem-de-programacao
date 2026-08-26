5a50e34c887bbe049212643a4a1972045e2c7d5d
Heitor Jasko <heitorjaskojogos@gmail.com>
2026-08-25 21:31:41 -0300

    exemplo1

---
+++ aula5/a.py
@@ -0,0 +1,23 @@
+lista = [10, 20, 30, 40, 50]
+
+# 2 ate 4
+print(lista[2:4])
+
+# 2 te o final
+print(lista[2:])
+
+
+# ate 4
+print(lista[:4])
+
+#td
+
+print(lista[:])
+
+
+print(lista[0:5:2])
+
+
+sublista = lista[1:4]
+
+print(sublista)
