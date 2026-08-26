84ca05723dbbf7315d9f03cdcada23285e18014f
Heitor Jasko <heitorjaskojogos@gmail.com>
2026-08-25 22:18:44 -0300

    exercicio

--- aula5/jexercicio1.py
+++ aula5/jexercicio1.py
@@ -11,7 +11,7 @@ while i == "y":
     largura = float(input("insira a largura: "))
     comprimento = float(input("insira o comprimento: "))
     resultado = (largura * comprimento)
-    print(f"seu comodo {nome} tem a area  {resultado}")
+    print(f"seu comodo {nome} tem a area de {resultado}")
     i = input("quer mais? Y para sim e N para não: ")
 
 print("obrigado por calcular .. ----")
